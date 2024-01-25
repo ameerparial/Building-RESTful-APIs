b8c6e661e10036857dbf5397e26a0b67c76dc8f6
ameerparial <khuhroameerhamza4@gmail.com>
2024-01-25 12:12:55 +0500

    Use body parser in app server.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import myroute from './src/routes/homeroute';
 import con_route from './src/routes/contactroute';
+import bodyParser from 'body-parser';
 const app = express();
 const PORT = 3000;
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mydb', {
 }).catch((err)=>{
     console.log("Error");
 });
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 myroute(app);
