e302291c00b1f2b46d7b74902f72cd755e87469e
ameerparial <khuhroameerhamza4@gmail.com>
2024-01-25 11:17:38 +0500

    Install a mongoose.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import myroute from './src/routes/homeroute';
 import con_route from './src/routes/contactroute';
 const app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/mydb', {
+    
+}).then((res)=>{
+    console.log("Connected");
+}).catch((err)=>{
+    console.log("Error");
+});
+
 
 myroute(app);
 
